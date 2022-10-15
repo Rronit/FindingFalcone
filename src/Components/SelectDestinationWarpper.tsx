dab8e718e71099af35fe07d697961b65438e3b04
Rronit <rronit2015@gmail.com>
2022-10-15 12:50:33 +0530

    feat: store read success

--- src/Components/SelectDestinationWarpper.tsx
+++ src/Components/SelectDestinationWarpper.tsx
@@ -1,1 +1,14 @@
-export {};
+import React, { useEffect } from "react";
+
+import { useSelector } from "react-redux";
+
+export const SelectDestinationWrapper = () => {
+  const planetDetails = useSelector((state: any) => state.planetDetails);
+  useEffect(() => {
+    console.log(planetDetails);
+  }, [planetDetails]);
+
+  // useEffect(() => {}, []);
+
+  return <>hello</>;
+};
