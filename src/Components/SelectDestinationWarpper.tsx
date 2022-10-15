bdbac1b51c197084e9019e472ba4bf1dc7b6d621
Rronit <rronit2015@gmail.com>
2022-10-15 22:13:05 +0530

    feat: redux write

--- src/Components/SelectDestinationWarpper.tsx
+++ src/Components/SelectDestinationWarpper.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import { useSelector } from "react-redux";
+import {
+  getPlanetDetails,
+  getVehicleDetails
+} from "../store/falcone.actionCreators";
 
 export const SelectDestinationWrapper = () => {
+  const dispatch = useDispatch();
   const planetDetails = useSelector((state: any) => state.planetDetails);
   useEffect(() => {
     console.log(planetDetails);
   }, [planetDetails]);
 
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    getPlanetDetails(dispatch);
+    getVehicleDetails(dispatch);
+  }, [dispatch]);
 
   return <>hello</>;
 };
--- src/store/falcone.actionCreators.ts
+++ src/store/falcone.actionCreators.ts
@@ -1,6 +1,26 @@
+import axios from "axios";
 import { falconeActions } from "./falcone.reducers";
+const baseURL = "https://findfalcone.herokuapp.com";
 
-export const getPlanetDetails = () => {};
+export const getPlanetDetails = async (dispatch: any) => {
+  const url = `${baseURL}/planets`;
+  try {
+    let resp = (await axios.get(url)).data;
+    dispatch(setPlanet(resp));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getVehicleDetails = async (dispatch: any) => {
+  const url = `${baseURL}/vehicles`;
+  try {
+    let resp = (await axios.get(url)).data;
+    dispatch(setPlanet(resp));
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export const setPlanet = (data: any) => {
   return async (dispatch: any) => {
